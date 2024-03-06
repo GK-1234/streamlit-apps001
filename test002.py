f51b7a1e8f03f94f3d3f8c1aae54f180bf540d3d
GK Kasa <gk.stylus@gmail.com>
2024-03-06 07:18:19 -0500

    Create test002.py

---
+++ test002.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langchain.llms import OpenAI
+#from langchain_community.llms import OpenAI
+
+st.title(' Stylus Test App')
+openai_api_key = st.sidebar.text_input('Open API key:')
+
+
+def generate_response(input_text):
+    lim = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(lim(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text: ', 'Give me use case and ask for test cases')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sm-'):
+        st.warning('Please enter your OpenAI Api key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
